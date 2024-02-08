ba0114d323acb8952bb0f5b2197e6ccaa090b9fa
Panos15081985 <damianakis.p@gmail.com>
2024-02-08 13:21:48 +0100

    Update firebase.js

--- src/Database/firebase.js
+++ src/Database/firebase.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCN2mscHx1hmYGc0glUO2wAoIL5kMsPJfw",
-    authDomain: "carpetdiem-13233.firebaseapp.com",
-    databaseURL: "https://carpetdiem-13233-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "carpetdiem-13233",
-    storageBucket: "carpetdiem-13233.appspot.com",
-    messagingSenderId: "693484254179",
-    appId: "1:693484254179:web:887d26b1d3654b61a526d6",
-    measurementId: "G-V79WHBYPDQ"
+  f//firebase Daten
   };
 
   const app = initializeApp(firebaseConfig);
